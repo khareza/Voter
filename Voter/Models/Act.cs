9f92c40c3e9d2d4192b1eb88f9f09a2d74240c85
Kamil Hareza <kml.hareza@gmail.com>
2019-05-16 13:25:41 +0200

    Add models with properties

---
+++ Voter/Models/Act.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Voter.Models
+{
+    public class Act
+    {
+        public int Id { get; set; }
+        public string ActNumber { get; set; }
+        public string Description { get; set; }
+        public List<User> Voters { get; set; }
+
+    }
+}
---
+++ Voter/Models/UserAct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Voter.Models
+{
+    public enum ActAnswer
+    {
+        For,
+        Against,
+        Hold
+    }
+
+    public class UserAct
+    {
+        public int Id { get; set; }
+        public User Voter { get; set; }
+        public Act Act { get; set; }
+        public DateTime VoteDate { get; set; }
+        public ActAnswer Answer { get; set; }
+    }
+}
