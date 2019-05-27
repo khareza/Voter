b6581d02f1cb6a21e4f2f6000a50eba6868dbfbe
unknown <kml.hareza@gmail.com>
2019-05-27 19:36:33 +0200

    Changed admin resolution css styles

--- Voter/ClientApp/src/components/Resolutions/Details/AdminResolution.jsx
+++ Voter/ClientApp/src/components/Resolutions/Details/AdminResolution.jsx
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import Moment from 'react-moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faFolderMinus } from '@fortawesome/free-solid-svg-icons'
 
@@ -18,11 +19,12 @@ export class AdminResolution extends Component {
             <div className="userCard">
                 <div className="userCardHeader">
                     <p>{`${this.props.resolution.title}`}</p>
-                    <p>Expiration Date: {this.props.resolution.expirationDate}</p>
+                    <p>Expiration Date: <Moment format="dddd YYYY-MM-DD HH:mm">{this.props.resolution.expirationDate}</Moment></p>
+                    <p>Voting ends <Moment fromNow="dddd YYYY-MM-DD HH:mm">{this.props.resolution.expirationDate}</Moment></p>
                 </div>
                 <div className="userData">
                     <div>
-                        <p><span>{this.props.resolution.resolutionNumber}</span> {this.props.resolution.description}</p>
+                        <p><span>Resolution No. {this.props.resolution.resolutionNumber}</span> {this.props.resolution.description}</p>
                     </div>
                     <div className="userButtons">
                         <a className="button buttonEdit" onClick={this.editResolution}><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></a>
